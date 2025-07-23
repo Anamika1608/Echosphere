432b2ca8aed43165e82c661fc430ce978d648318
ashree2118 <ashree2118@gmail.com>
2025-07-24 00:17:49 +0530

    installed shadcn and started frontend ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Button } from "@/components/ui/button"
 
 
 import './App.css'
@@ -9,8 +10,9 @@ function App() {
   return (
     <>
       <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-        <h1 className='text-4xl font-bold mb-4'>Welcome to My App</h1>
-        <p className='text-lg text-gray-700'>This is a simple React app styled with Tailwind CSS.</p>
+        <h1 className='text-4xl font-bold '>Welcome to My App</h1>
+        <p className='text-lg text-gray-700 py-4'>This is a simple React app styled with Tailwind CSS.</p>
+        <Button>Click me</Button>
       </div>
       
     </>
